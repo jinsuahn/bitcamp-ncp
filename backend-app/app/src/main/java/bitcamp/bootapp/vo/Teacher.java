b3e8353ae5ab07e61509d22a354d077b57ce5608
jskozy <jinsu8596@gmail.com>
2023-01-12 20:31:04 +0900

    type : Java day-14
    - 자바(계속)
        - 상속 사용법
        - 실전 백엔드 프로젝트(backend-app)
            - 04.객체 생성 및 주입 자동화, 스프링부트 설정 방식
            - 05.상속 활용

--- backend-app/app/src/main/java/bitcamp/bootapp/vo/Teacher.java
+++ backend-app/app/src/main/java/bitcamp/bootapp/vo/Teacher.java
@@ -1,69 +1,43 @@
 package bitcamp.bootapp.vo;
 
-// 회원 데이터를 담을 메모리를 설계한다.
-public class Teacher {
-	private int no;
-	private String name;
-	private String tel;
-	private String email;
-	private byte degree;
-	private String school;
-	private String major;
-	private String wage;
-	private String createdDate;
+public class Teacher extends Member {
+  // Member 클래스의 코드를 사용하겠다고 선언한다.
 
-	public int getNo() {
-		return no;
-	}
-	public void setNo(int no) {
-		this.no = no;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public byte getDegree() {
-		return degree;
-	}
-	public void setDegree(byte degree) {
-		this.degree = degree;
-	}
-	public String getSchool() {
-		return school;
-	}
-	public void setSchool(String school) {
-		this.school = school;
-	}
-	public String getMajor() {
-		return major;
-	}
-	public void setMajor(String major) {
-		this.major = major;
-	}
-	public String getWage() {
-		return wage;
-	}
-	public void setWage(String wage) {
-		this.wage = wage;
-	}
-	public String getCreatedDate() {
-		return createdDate;
-	}
-	public void setCreatedDate(String createdDate) {
-		this.createdDate = createdDate;
-	}
+  private String email;
+  private int degree;
+  private String school;
+  private String major;
+  private int wage;
+
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public int getDegree() {
+    return degree;
+  }
+  public void setDegree(int degree) {
+    this.degree = degree;
+  }
+  public String getSchool() {
+    return school;
+  }
+  public void setSchool(String school) {
+    this.school = school;
+  }
+  public String getMajor() {
+    return major;
+  }
+  public void setMajor(String major) {
+    this.major = major;
+  }
+  public int getWage() {
+    return wage;
+  }
+  public void setWage(int wage) {
+    this.wage = wage;
+  }
+
 }
