9cfcaec938d155c7778932ace4b6bd4c21ee2fab
jskozy <jinsu8596@gmail.com>
2023-02-23 23:42:52 +0900

    Subject : [BITCAMP] Naver Cloud AIaaS Class - 230223(Thu)
     - JAVA 문법 응용 실전 프로젝트(myapp)
      - 50.파일을 업로드하기: multipart/form-data MIME 타입 다루기

--- myapp/app-server/src/main/java/bitcamp/myapp/filter/AuthFilter.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/filter/AuthFilter.java
@@ -14,30 +14,37 @@ import bitcamp.myapp.vo.Member;
 @WebFilter("/*")
 public class AuthFilter implements Filter {
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
 
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-		String contextRoot = httpRequest.getContextPath();
-		String servletPath = httpRequest.getServletPath().toLowerCase();
+    String contextRoot = httpRequest.getContextPath();
+    String servletPath = httpRequest.getServletPath().toLowerCase();
 
-		if (servletPath.endsWith("insert") ||
-				servletPath.endsWith("update") ||
-				servletPath.endsWith("delete")) {
-			// 로그인 사용자의 정보를 가져온다.
-			Member loginUser = (Member) httpRequest.getSession().getAttribute("loginUser");
-			if (loginUser == null) {
-				httpResponse.sendRedirect(contextRoot + "/auth/form");
-				return;
-			}
-		}
+    if (servletPath.endsWith("insert") ||
+        servletPath.endsWith("update") ||
+        servletPath.endsWith("delete")) {
+      // 로그인 사용자의 정보를 가져온다.
+      Member loginUser = (Member) httpRequest.getSession().getAttribute("loginUser");
+      if (loginUser == null) {
+        httpResponse.sendRedirect(contextRoot + "/auth/form");
+        return;
+      }
+    }
 
-
-		chain.doFilter(request, response); // 다음 필터 실행. 다음 필터가 없으면 최종 목적지인 서블릿 실행.
-
-
-	}
+    chain.doFilter(request, response); // 다음 필터 실행. 다음 필터가 없으면 최종 목적지인 서블릿 실행.
+  }
 }
+
+
+
+
+
+
+
+
+
+
