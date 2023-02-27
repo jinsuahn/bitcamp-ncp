b7420c07da2ca76377e3801a2bef2a239669a195
jskozy <jinsu8596@gmail.com>
2023-02-27 19:17:30 +0900

    Subject : 230227(Mon)
    Java 문법 응용 실전 프로젝트(myapp)
     - 54.페이지 컨트롤러 생성 자동화: Reflection API 활용
     - 55.애노테이션을 활용하여 페이지 컨트롤러의 경로 지정하기
     - 56.페이지 컨트롤러 생성 자동화 II: 애노테이션 활용
     - 57.페이지 컨트롤러의 요청 핸들러를 지정하기: 애노테이션 활용
     - 58.요청 핸들러의 파라미터 값 자동으로 주입하기: 애노테이션 활용

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/DownloadController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/DownloadController.java
@@ -1,43 +1,40 @@
-package bitcamp.myapp.servlet;
+package bitcamp.myapp.controller;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import bitcamp.myapp.service.BoardService;
 import bitcamp.myapp.vo.BoardFile;
+import bitcamp.util.Controller;
+import bitcamp.util.RequestMapping;
+import bitcamp.util.RequestParam;
 
-@WebServlet("/download/boardfile")
-public class DownloadServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller
+public class DownloadController {
 
   private BoardService boardService;
 
-  @Override
-  public void init() {
-    boardService = (BoardService) getServletContext().getAttribute("boardService");
+  public DownloadController(BoardService boardService) {
+    this.boardService = boardService;
   }
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+  @RequestMapping("/download/boardfile")
+  public String execute(
+      @RequestParam("fileNo") int fileNo,
+      HttpServletRequest request,
+      HttpServletResponse response) {
 
     try {
-      int fileNo = Integer.parseInt(request.getParameter("fileNo"));
-
       BoardFile boardFile = boardService.getFile(fileNo);
       if (boardFile == null) {
         throw new RuntimeException("파일 정보 없음!");
       }
 
       File downloadFile = new File(
-          this.getServletContext().getRealPath("/board/upload/" + boardFile.getFilepath()));
+          request.getServletContext().getRealPath("/board/upload/" + boardFile.getFilepath()));
       if (!downloadFile.exists()) {
         throw new RuntimeException("파일이 존재하지 않음!");
       }
@@ -59,9 +56,10 @@ public class DownloadServlet extends HttpServlet {
       }
 
     } catch (Exception e) {
-      request.getRequestDispatcher("/downloadfail.jsp").forward(request, response);
       e.printStackTrace();
+      return "/downloadfail.jsp";
     }
+    return null;
   }
 }
 
